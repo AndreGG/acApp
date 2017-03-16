eba093efff3529eaa0cede045fba70ccb7660e40
André Leal <andre.g.leal@gmail.com>
2017-03-16 15:55:01 +0000

    Hibernate implementation

--- src/main/java/org/academiadecodigo/bootcamp/hackathon/persistence/DBConnectionManager.java
+++ src/main/java/org/academiadecodigo/bootcamp/hackathon/persistence/DBConnectionManager.java
@@ -3,5 +3,5 @@ package org.academiadecodigo.bootcamp.hackathon.persistence;
 /**
  * Created by codecadet on 3/16/17.
  */
-public interface dbConnectorManager {
+public interface DBConnectionManager {
 }
--- src/main/java/org/academiadecodigo/bootcamp/hackathon/services/ServiceInitializer.java
+++ src/main/java/org/academiadecodigo/bootcamp/hackathon/services/ServiceInitializer.java
@@ -1,7 +1,33 @@
 package org.academiadecodigo.bootcamp.hackathon.services;
 
+import org.academiadecodigo.bootcamp.hackathon.model.dao.BootcampDao;
+import org.academiadecodigo.bootcamp.hackathon.model.dao.CadetDao;
+import org.academiadecodigo.bootcamp.hackathon.model.dao.SummarizerDao;
+import org.academiadecodigo.bootcamp.hackathon.persistence.DBConnectionManager;
+import org.academiadecodigo.bootcamp.hackathon.persistence.hibernate.HibernateSessionManager;
+
 /**
  * Created by codecadet on 3/16/17.
  */
 public class ServiceInitializer {
+
+    private DBConnectionManager dbConnectionManager;
+
+    public void init() {
+
+        HibernateSessionManager hibernateManager = new HibernateSessionManager();
+        CadetDao cadetDAO = ;
+        BootcampDao bootcampDAO = ;
+        SummarizerDao summarizerDao = ;
+        
+
+        ServiceRegistry.getInstance().registerService(userService);
+
+        this.dbConnectionManager = hibernateManager;
+    }
+
+    public void stopServices() {
+        dbConnectionManager.close();
+    }
+
 }
