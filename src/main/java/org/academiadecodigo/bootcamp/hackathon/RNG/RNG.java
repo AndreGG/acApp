66d347c03dcbbf45fc5d0e954966f806c7648355
André Leal <andre.g.leal@gmail.com>
2017-03-16 18:19:51 +0000

    seating assignment service

---
+++ src/main/java/org/academiadecodigo/bootcamp/hackathon/RNG/RNG.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.bootcamp.hackathon.RNG;
+
+/**
+ * Created by codecadet on 3/16/17.
+ */
+public final class RNG {
+
+    public static int roll() {
+        return (int)(Math.random() * 100);
+    }
+
+}
--- src/main/java/org/academiadecodigo/bootcamp/hackathon/services/SeatingAssignmentService.java
+++ src/main/java/org/academiadecodigo/bootcamp/hackathon/services/SeatingAssignmentService.java
@@ -1,9 +1,10 @@
 package org.academiadecodigo.bootcamp.hackathon.services;
 
+import org.academiadecodigo.bootcamp.hackathon.RNG.RNG;
 import org.academiadecodigo.bootcamp.hackathon.model.Cadet;
 import org.academiadecodigo.bootcamp.hackathon.model.dao.BootcampDao;
 import org.academiadecodigo.bootcamp.hackathon.model.dao.CadetDao;
-import org.hibernate.type.IntegerType;
+import org.academiadecodigo.bootcamp.hackathon.persistence.file.SettingsFileHandler;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,22 +17,68 @@ public abstract class SeatingAssignmentService {
     private CadetDao cadetDao;
     private BootcampDao bootcampDao;
     private List<Cadet> cadets;
-    private HashMap<String, Integer> currentSeatAssignment;
+    private HashMap<String, Integer> riggedSeats;
+    private int seatsByRow;
 
     public SeatingAssignmentService(CadetDao cadetDao, BootcampDao bootcamp) {
 
         this.cadetDao = cadetDao;
         this.bootcampDao = bootcamp;
         this.cadets = bootcampDao.getCadets();
+        seatsByRow = SettingsFileHandler.getSeatsByRow();
 
         for(Cadet cadet: cadets) {
             currentSeatAssignment.put(cadet.getName(), cadet.getCurrentSeat());
         }
 
-        currentSeatAssignment.containsKey()
-
     }
 
-    public void
+    public Cadet assignSeat(int seatNumber) {
+
+        if(isRigged(seatNumber)) {
+
+            String name = SettingsFileHandler.getCadetName();
+            Cadet riggedCadet = cadetDao.findByName(name);
+            riggedCadet.setCurrentSeat(seatNumber);
+
+            cadets.remove(riggedCadet);
+            return riggedCadet;
+
+        } else {
+
+            int highestRoll = 0;
+            Cadet assignedCadet = null;
+
+            for(Cadet cadet: cadets) {
+
+                double currentSeat = cadet.getCurrentSeat();
+                int chance = getCadetRow(currentSeat);
+
+                int cadetRoll = RNG.roll() * chance;
+
+                if(cadetRoll > highestRoll) {
+
+                    highestRoll = cadetRoll;
+                    assignedCadet = cadet;
+
+                }
+
+            }
+
+            cadets.remove(assignedCadet);
+            return assignedCadet;
+        }
+    }
+
+    public int getCadetRow(double cadetSeat) {
+        return (int)Math.ceil(cadetSeat/seatsByRow);
+    }
+
+    public boolean isRigged(int seatNumber) {
+
+        boolean rigged = false;
+
+        return rigged;
+    }
 
 }
