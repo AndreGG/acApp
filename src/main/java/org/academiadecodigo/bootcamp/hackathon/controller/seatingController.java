91358257a7ab6f9c45a7a199de5cfa1c990c0ff9
António Almeida <antonio_m_almeida@hotmail.com>
2017-03-17 04:59:08 +0000

    Testing tableView in seatingController

--- src/main/java/org/academiadecodigo/bootcamp/hackathon/controller/seatingController.java
+++ src/main/java/org/academiadecodigo/bootcamp/hackathon/controller/seatingController.java
@@ -1,7 +1,51 @@
 package org.academiadecodigo.bootcamp.hackathon.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import org.academiadecodigo.bootcamp.hackathon.model.Cadet;
+
 
 /**
  * Created by codecadet on 3/16/17.
  */
 public class seatingController {
+
+
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private TableView<Cadet> cadetsList;
+
+    @FXML
+    private TableColumn<Cadet, String> columnCadets;
+
+    @FXML private void initialize() {
+        columnCadets.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        cadetsList.getItems().setAll(parseCadetList());
+    }
+
+    private Cadet parseCadetList() {
+        return null;
+    }
+
+    @FXML
+    void showAdminSettings(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showSeating(ActionEvent event) {
+
+    }
+
+
+
+
 }
